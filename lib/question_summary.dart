6c07b530e9702f8ceb191ab23dd0ea5c5d84708d
Sabbha20 <sabbha.power1@gmail.com>
2024-03-04 23:45:19 +0530

    Mapped question index with the List<Map<String, Object>> in the Column Widget

--- lib/question_summary.dart
+++ lib/question_summary.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 class QuestionSummary extends StatelessWidget {
-  const QuestionSummary({super.key});
+  const QuestionSummary(this.summaryData, {super.key});
+
+  final List<Map<String, Object>> summaryData;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: summaryData
+          .map(
+            (data) => Row(
+              children: [
+                Text(((data['question_index'] as int) + 1).toString()),
+              ],
+            ),
+          )
+          .toList(),
+    );
   }
 }
